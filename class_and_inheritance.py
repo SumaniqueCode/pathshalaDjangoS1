8d2b3855916d05772c4a271835e2fd9433c1ec11
Suman Regmi <limitlessplaying@gmail.com>
2025-05-27 12:31:21 +0545

    Class and inheritance

--- class_and_inheritance.py
+++ class_and_inheritance.py
@@ -2,19 +2,24 @@
 class A:
     def display_name():
         print("This is class A")
-class B(A): # class B inherits from class A
+
+
+class B(A):  # class B inherits from class A
     def print_name():
         print("This is class B")
-class C(B): # class C is derived from class B
+
+
+class C(B):  # class C is derived from class B
     def print_data():
         print("This is class C")
-        
-# obj = A
-# obj.display_name()
 
-# obj2 = B
-# obj2.print_name()
-# obj2.display_name()
+
+obj = A
+obj.display_name()
+
+obj2 = B
+obj2.print_name()
+obj2.display_name()
 
 obj3 = C
 obj3.print_data()
