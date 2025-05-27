74af6c63527b6f8d8ba0fa17b32c253a16d2f190
Suman Regmi <limitlessplaying@gmail.com>
2025-05-27 12:30:58 +0545

    Class and inheritance

---
+++ class_and_inheritance.py
@@ -0,0 +1,22 @@
+# basic class
+class A:
+    def display_name():
+        print("This is class A")
+class B(A): # class B inherits from class A
+    def print_name():
+        print("This is class B")
+class C(B): # class C is derived from class B
+    def print_data():
+        print("This is class C")
+        
+# obj = A
+# obj.display_name()
+
+# obj2 = B
+# obj2.print_name()
+# obj2.display_name()
+
+obj3 = C
+obj3.print_data()
+obj3.print_name()
+obj3.display_name()
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-def sum(num1, num2):
-#     add = num1 + num2
-#     print("The sum is ", add)
-
-# a=10
-# b=21
-# sum(a, b)
