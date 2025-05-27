2603ccd9b20b5faa5fd8420840ea85a778be0952
Suman Regmi <limitlessplaying@gmail.com>
2025-05-27 12:54:44 +0545

    class, constructor and inheritance

--- class_and_inheritance.py
+++ class_and_inheritance.py
@@ -12,14 +12,28 @@ class C(B):  # class C is derived from class B, multi-level inheritance
     def print_data():
         print("This is class C")
 
-obj = A
-obj.display_name()
+class Sum:
+    #constructor
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    
+    def add(self):
+        result= self.num1 + self.num2
+        return result
+        
 
-obj2 = B
-obj2.print_name()
-obj2.display_name()
+# obj = A
+# obj.display_name()
 
-obj3 = C
-obj3.print_data()
-obj3.print_name()
-obj3.display_name()
+# obj2 = B
+# obj2.print_name()
+# obj2.display_name()
+
+# obj3 = C
+# obj3.print_data()
+# obj3.print_name()
+# obj3.display_name()
+
+obj4 = Sum(1,3)
+print(obj4.add())
